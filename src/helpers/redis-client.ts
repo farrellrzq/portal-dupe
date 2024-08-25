0d1b3fdba1665c80333155d1cc22f3f5bdb9f9ae
root <root@portalopd.depok.go.id>
2024-08-26 02:59:14 +0700

    chore (redis-client): remove username in kredensial redis

--- src/helpers/redis-client.ts
+++ src/helpers/redis-client.ts
@@ -3,8 +3,7 @@ import Redis from 'ioredis';
 const redis =  new Redis({
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),  
-        db: Number(process.env.REDIS_DB), 
-        username: process.env.REDIS_USERNAME,
+        db: Number(process.env.REDIS_DB),
         password: process.env.REDIS_PASSWORD,
 });
 
