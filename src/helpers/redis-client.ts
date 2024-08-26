82d325566bbd14ed8abe12f46e4dbcfaf2ec65a0
RaffiRmdhn <klsterbuka@gmail.com>
2024-08-26 16:14:25 +0700

    Fix layanan kota homepage lazy loading

--- src/helpers/redis-client.ts
+++ src/helpers/redis-client.ts
@@ -4,8 +4,8 @@ const redis =  new Redis({
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),  
         db: Number(process.env.REDIS_DB), 
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
+        // username: process.env.REDIS_USERNAME,
+        // password: process.env.REDIS_PASSWORD,
 });
 
 export default redis;
