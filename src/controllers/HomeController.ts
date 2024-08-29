2b6e8b97f83181aee94964ac15ce67542dbff98a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-08-29 10:27:49 +0700

    clean : remove code unused in HomeController

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,16 +1,13 @@
 import { consoleError, API_CMS, API_DSW, API_BERITA_DEPOK } from "@/helpers/site";
 import { api,  getDomainSite } from "./Controller";
 import { 
-  BeritaKelurahanProps, 
   BeritaKotaProps, 
   BeritaProps, 
   DokumenProps, 
   InfografisProps, 
-  KomoditasProps, 
   LayananKotaProps, 
   LayananProps, 
   PengumumanProps, 
-  PotensiProps, 
   SliderProps
 } from "./types/home-controller.type";
 import  redis from '@/helpers/redis-client';
@@ -46,11 +43,11 @@ export async function getSlider() {
   let Slider: SliderProps[] | null = null;
   const cachedKey=`slider_id:${Id}`;
 
-  const cachedResult=await redisGetString(redis,cachedKey);
+  const cachedResult=await redisGetList(redis, cachedKey);
 
-  if (cachedResult) {
-    Slider=JSON.parse(cachedResult);
-    return Slider;
+  if (cachedResult.length > 0) {
+     Slider = cachedResult.map(item => JSON.parse(item)) as SliderProps[];
+     return Slider;
   }
 
   const result = await api({ url: `${API_CMS}/ViewPortal/getSlider?siteId=${Id}&typeId=SL01&status=ST01&fileType=FL02` });
@@ -58,11 +55,15 @@ export async function getSlider() {
   if ('error' in result) {
     consoleError('getSlider()', result.error);
   } else {
-    Slider = result;
+    Slider = result ? result : [];
   }
 
-  await redisSaveString(redis,cachedKey, 3600, JSON.stringify(result));
-
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
+  
   return Slider;
 }
 
@@ -72,22 +73,26 @@ export async function getLayanan() {
   
   const cachedKey=`layanan_id:${Id}`;
 
-  const cachedResult=await redisGetString(redis,cachedKey);
+  const cachedResult=await redisGetList(redis, cachedKey);
 
-  if (cachedResult) {
-    Layanan=JSON.parse(cachedResult);
-    return Layanan;
-  }
+  if (cachedResult.length > 0) {
+     Layanan = cachedResult.map(item => JSON.parse(item)) as LayananProps[];
+     return Layanan;
+  } 
 
   const result = await api({ url: `${API_CMS}/ViewPortal/get_content?siteId=${Id}&kanalType=K010&limit=&offset=&category=&slug=&key=&groupId=Aplikasi` });
 
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
-    Layanan = result;
+    Layanan = result ? result : [];
   }
 
-  await redisSaveString(redis, cachedKey, 3600, JSON.stringify(result));
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
 
   return Layanan;
 }
@@ -97,10 +102,10 @@ export async function getLayananKota() {
 
   const cachedKey=`layanan_kota`;
 
-  const cachedResult=await redisGetString(redis,cachedKey);
+  const cachedResult=await redisGetList(redis,cachedKey);
 
-  if (cachedResult) {
-    LayananKota=JSON.parse(cachedResult);
+  if (cachedResult.length > 0) {
+    LayananKota = cachedResult.map(item => JSON.parse(item)) as LayananKotaProps[];
     return LayananKota;
   }
 
@@ -109,11 +114,15 @@ export async function getLayananKota() {
   if ('error' in result) {
     consoleError('getExLink()', result.error);
   } else {
-    LayananKota = result;
+    LayananKota = result ? result : [];
   }
 
-  await redisSaveString(redis, cachedKey, 3600, JSON.stringify(result));
-
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
+  
   return LayananKota;
 }
 
@@ -122,13 +131,27 @@ export async function getInfografis() {
 
   const result = await api({ url: `${API_DSW}/index.php/api/slider` });
  
+  const cachedKey=`infografis`;
+
+  const cachedResult=await redisGetList(redis,cachedKey);
+
+  if (cachedResult.length > 0) {
+    Infografis = cachedResult.map(item => JSON.parse(item)) as InfografisProps[];
+    return Infografis;
+  }
+
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
     Infografis = result ? result.data : [];
   }
 
-
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
+  
   return Infografis;
 }
 
@@ -138,22 +161,26 @@ export async function getDokumen() {
 
   const cachedKey=`dokumen_id:${Id}`;
 
-  const cachedResult=await redisGetString(redis,cachedKey);
+  const cachedResult=await redisGetList(redis,cachedKey);
 
-  if (cachedResult) {
-    Dokumen=JSON.parse(cachedResult);
-    return Dokumen;
+   if (cachedResult.length > 0) {
+     Dokumen = cachedResult.map(item => JSON.parse(item)) as DokumenProps[];
+     return Dokumen;
   }
 
-  const result = await api({ url: `${API_CMS}/ViewPortal/get_content?siteId=${Id}&status=ST01&kanalType=K010&limit=3` });
+  const result = await api({ url: `${API_CMS}/ViewPortal/get_content?siteId=${Id}&status=ST01&kanalType=K010&limit=3`});
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
-    Dokumen = result;
+    Dokumen = result ? result : [];
   }
 
-  await redisSaveString(redis, cachedKey, 3600, JSON.stringify(result));
-
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
+  
   return Dokumen;
 }
 
@@ -164,89 +191,58 @@ export async function getPengumuman() {
   const cachedKey=`pengumuman_id:${Id}`;
   const cachedResult=await redisGetList(redis, cachedKey);
 
-
   if (cachedResult.length > 0) {
      Pengumuman = cachedResult.map(item => JSON.parse(item)) as PengumumanProps[];
      return Pengumuman;
   }
+
   const result = await api({ url: `${API_CMS}/ViewPortal/get_content?siteId=${Id}&status=ST01&kanalType=K008&limit=3&offset=&category=&slug=&key=`});
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
-    Pengumuman = result;
+    Pengumuman = result ? result : [];
   }
 
-  result.forEach(async(data:any) => {
-    await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
-  });
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
 
   return Pengumuman;
 }
 
-export async function getKomoditas() {
-  let Komoditas: KomoditasProps[] | null = null;
-  const result = await api({ url: `${API_DSW}/api/komoditas/harga_depok` });
-  
-  if ('error' in result) {
-    consoleError('get_content()', result.error);
-  } else {
-    Komoditas = result.data;
-  }
-
-  const cachedKey=`komoditas`;
-
-  const cachedResult=await redisGetString(redis,cachedKey);
-
-  if (cachedResult) {
-    Komoditas=JSON.parse(cachedResult);
-    return Komoditas;
-  }
-
-  await redisSaveString(redis, cachedKey, 3600, JSON.stringify(result));
-
-  return Komoditas;
-}
-
-export async function getPotensi() {
-  const { Id } = await getDomainSite();
-  let Potensi: PotensiProps[] | null = null;
-
-  const cachedKey=`potensi_id:${Id}`;
-
-  const cachedResult=await redisGetString(redis,cachedKey);
-
-  if (cachedResult) {
-    Potensi=JSON.parse(cachedResult);
-    return Potensi;
-  }
-
-  const result = await api({ url: `${API_CMS}/ViewPortal/getPlace?siteId=${Id}&typeId=&limit=3&offset=` });
-  if ('error' in result) {
-    consoleError('get_content()', result.error);
-  } else {
-    Potensi = result;
-  }
-
-  await redisSaveString(redis, cachedKey, 3600, JSON.stringify(result));
-
-  return Potensi;
-}
-
 export async function getBeritaKota() {
   let BeritaKota: BeritaKotaProps[] | null = null;
+
+  const cachedKey=`berita_kota`;
+  const cachedResult=await redisGetList(redis, cachedKey);
+
+  if (cachedResult.length > 0) {
+     BeritaKota = cachedResult.map(item => JSON.parse(item)) as BeritaKotaProps[];
+     return BeritaKota;
+  }
+
   const result = await api({ url: `${API_BERITA_DEPOK}` });
+
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
-    BeritaKota = result;
+    BeritaKota = result ? result : [];
   }
+
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
+
   return BeritaKota;
 }
 
 export async function getBerita() {
   const { Id } = await getDomainSite();
   let Berita: BeritaProps[] | null = null;
-
 
   const cachedKey=`berita_id:${Id}`;
 
@@ -258,15 +254,18 @@ export async function getBerita() {
   }
 
   const result = await api({ url: `${API_CMS}/ViewPortal/get_content?siteId=${Id}&status=ST01&kanalType=K001&limit=3` });
+
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
-    Berita = result;
+    Berita = result ? result : [];
   }
 
-  result.forEach(async(data:any) => {
-    await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
-  });
+  if(result.length > 0){
+    result.forEach(async(data:any) => {
+      await redisSaveList(redis, cachedKey, 3600, JSON.stringify(data));
+    });
+  }
 
   return Berita;
 }
@@ -302,100 +301,4 @@ export async function getToken() {
     throw error;
   }
 }
-
-export async function getWidgetData() {
-  try {
-    const { token, url } = await getToken();
-    const body = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify({ tahun: '2023' })
-    };
-
-    const [response, disease] = await Promise.all([
-      fetch(`${url}/api/kependudukan/aggpenduduk/perjkel`, body),
-      fetch(`${url}/api/kesehatan/aggkesehatan/penyakitperjkel`, body)
-    ]);
-
-    if (!response.ok || !disease.ok) {
-      const errorMessage = `Fetching widget data failed with status: ${response.status}, ${disease.status}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    const resdsw = await fetch(`${API_DSW}/html/penyakitdata/`);
-    if (!resdsw.ok) {
-      const errorMessage = `Fetching penyakit data failed with status: ${resdsw.status}`;
-      console.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-
-    const jumlahPenyakit = await resdsw.json();
-    const dataPenyakit = jumlahPenyakit.data;
-    const getHighest = dataPenyakit.map((item: any) => item.total);
-    const sorten = formatterNumber(parseInt(getHighest.sort()[0]));
-
-    const Penyakit = jumlahPenyakit.data;
-    const getPenyakit = Penyakit.map((item: any) => item.penyakit);
-    const PenyakitName = getPenyakit;
-
-    const [dataAll, dataDisease] = await Promise.all([
-      response.json(),
-      disease.json()
-    ]);
-
-    const kepen = dataAll.data;
-    const yearPenduduk = kepen && kepen.length > 0 ? kepen[0].tahun : null; // Kondisi untuk menangani data null
-    const perjenis = groupPerjkel(kepen);
-    const total = formatterNumber(sumJumlah(kepen));
-    const totalPenyakit = formatterNumber(sumJumlah(dataDisease.data));
-    const totalMale = formatterNumber(sumJumlah(perjenis['Laki - Laki']));
-    const totalFemale = formatterNumber(sumJumlah(perjenis['Perempuan']));
-
-    return { total, totalMale, totalFemale, totalPenyakit, sorten, yearPenduduk, dataPenyakit, PenyakitName };
-  } catch (error) {
-    console.error('Error in getWidgetData:', error);
-    throw error;
-  }
-}
-
-export function sumJumlah(data: any) {
-  if (!Array.isArray(data) || data.length === 0) {
-    return 0;
-  }
-
-  const total = data.reduce((acc: number, obj: any) => {
-    const jumlah = obj.jumlah ? parseInt(obj.jumlah) : parseInt(obj.total);
-    return acc + jumlah;
-  }, 0);
-
-  return total;
-}
-
-function groupPerjkel(data: any) {
-  const groupedData: { [key: string]: typeof data } = {};
-
-  if (!Array.isArray(data) || data.length === 0) {
-    return groupedData;
-  }
-
-  data.forEach((item: any) => {
-    const { jenis_kelamin, ...rest } = item;
-    if (!groupedData[jenis_kelamin]) {
-      groupedData[jenis_kelamin] = [];
-    }
-    groupedData[jenis_kelamin].push(rest);
-  });
-
-  return groupedData;
-}
-
-
-export function formatterNumber(num: any) {
-  return num.toLocaleString('en-US', { maximumFractionDigits: 2 });
-}
-
 
