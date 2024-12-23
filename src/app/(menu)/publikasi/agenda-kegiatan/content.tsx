342dcab94ec40f512ee2ed649d7d665a9185e172
ciptadra <udin43641@gmail.com>
2024-12-23 15:43:26 +0700

    pengumuman search {urgent} Done agenda {mid} Done berita & artikel {low} Done

--- src/app/(menu)/publikasi/agenda-kegiatan/content.tsx
+++ src/app/(menu)/publikasi/agenda-kegiatan/content.tsx
@@ -50,33 +50,33 @@ export default function Content({ agenda }: { agenda: AgendaProps[] | null }) {
     <>
       <section className="relative bg-cover bg-center bg-no-repeat py-12">
         <div className="container relative z-10">
-          <h2 className="mb-6 font-display text-3xl text-jacarta-300">Agenda</h2>
-          <p className="mb-12 max-w-xl text-lg text-jacarta-300">Agenda OPD</p>
+          <h2 className="mb-6 font-display text-3xl text-gray-900 text-jacarta-700 dark:text-jacarta-300">Agenda</h2>
+          <p className="mb-12 max-w-xl text-lg text-gray-900 text-jacarta-700 dark:text-jacarta-300">Agenda OPD</p>
 
           {/* Tahun Filter */}
           <div className="flex">
             <ul className="nav nav-tabs w-1/3 space-y-9 self-start border-l-2 border-jacarta-200 py-2 pl-2 md:pl-8" role="tablist">
               <li className="nav-item" role="presentation">
                 <button
-                  className={`nav-link nav-link--style-3 relative flex items-center whitespace-nowrap ${!selectedYear ? 'active' : ''} text-jacarta-300 hover:text-black`}
+                  className={`nav-link nav-link--style-3 relative flex items-center whitespace-nowrap ${!selectedYear ? 'active' : ''} text-gray-900 text-jacarta-700 dark:text-jacarta-300 hover:text-black`}
                   id="semua-tab"
                   type="button"
                   onClick={() => handleYearChange('')}
                   aria-selected={!selectedYear}
                 >
-                  <span className="px-2 font-display text-lg font-medium md:text-2xl text-jacarta-300">Semua Tahun</span>
+                  <span className="px-2 font-display text-lg font-medium md:text-2xl text-gray-900 text-jacarta-700 dark:text-jacarta-300">Semua Tahun</span>
                 </button>
               </li>
               {['2024', '2023', '2022', '2021', '2020'].map((year) => (
                 <li className="nav-item" key={year} role="presentation">
                   <button
-                    className={`nav-link nav-link--style-3 relative flex items-center whitespace-nowrap ${selectedYear === year ? 'active' : ''} text-jacarta-300 hover:text-black`}
+                    className={`nav-link nav-link--style-3 relative flex items-center whitespace-nowrap ${selectedYear === year ? 'active' : ''} text-gray-900 text-jacarta-700 dark:text-jacarta-300 hover:text-black`}
                     id={`${year}-tab`}
                     type="button"
                     onClick={() => handleYearChange(year)}
                     aria-selected={selectedYear === year}
                   >
-                    <span className="px-2 font-display text-lg font-medium md:text-2xl text-jacarta-300">{year}</span>
+                    <span className="px-2 font-display text-lg font-medium md:text-2xl text-gray-900 text-jacarta-700 dark:text-jacarta-300">{year}</span>
                   </button>
                 </li>
               ))}
@@ -86,45 +86,40 @@ export default function Content({ agenda }: { agenda: AgendaProps[] | null }) {
             <div className="tab-content w-full pl-4 md:px-16 md:w-2/4">
               <div className="tab-pane px-10 fade active show" id="semua" role="tabpanel">
                 <div className="relative">
-                  <div className="swiper card-slider-2-columns !pb-5">
-                    <div className="swiper-wrapper">
-                      {currentAgenda.length > 0 ? (
-                        currentAgenda.map((item: any, index: number) => (
-                          <div className="swiper-slide" key={index}>
-                            <article>
-                              <div className="block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700">
-                                <figure className="relative">
-                                  <a href="#">
-                                    <img
-                                      src={item.Media ? `https://cms.depok.go.id/upload/event/${item.Media}` : "/img/kecamatan/dsw/kesehatan.png"}
-                                      alt={item.Title}
-                                      height="430"
-                                      width="379"
-                                      className="swiper-lazy h-[430px] w-full object-cover brightness-50"
-                                    />
-                                    <div className="swiper-lazy-preloader"></div>
-                                  </a>
-                                </figure>
-                                <div className="p-6">
-                                  <div className="flex items-center justify-between">
-                                    <a href="#">
-                                      <span className="font-display text-base text-jacarta-700 dark:text-white">{formatTanggal(item.TanggalAwal)}</span>
-                                    </a>
-                                    <span className="flex items-center whitespace-nowrap rounded-md border border-jacarta-100 py-1 px-2">
-                                      <span className="text-sm font-medium tracking-tight text-gray dark:text-white">Sudah Berlalu</span>
-                                    </span>
-                                  </div>
-                                  <h2 className="mb-4 mt-2 font-display text-xl text-jacarta-700 dark:text-white">{item.Title}</h2>
-                                  <p className="text-sm text-jacarta-200 dark:text-white">{item.Deskripsi.slice(0, 150) + '...'}</p>
-                                </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                    {currentAgenda.length > 0 ? (
+                      currentAgenda.map((item: any, index: number) => (
+                        <article key={index}>
+                          <div className="block overflow-hidden rounded-2.5xl bg-white shadow-md transition-shadow hover:shadow-lg dark:bg-jacarta-700">
+                            <figure className="relative">
+                              <a href="#">
+                                <img
+                                  src={item.Media ? `https://cms.depok.go.id/upload/event/${item.Media}` : "/img/kecamatan/dsw/kesehatan.png"}
+                                  alt={item.Title}
+                                  height="430"
+                                  width="379"
+                                  className="h-[230px] w-full object-cover brightness-50"
+                                />
+                              </a>
+                            </figure>
+                            <div className="p-6">
+                              <div className="flex items-center justify-between">
+                                <a href="#">
+                                  <span className="font-display text-base text-jacarta-700 dark:text-white">{formatTanggal(item.TanggalAwal)}</span>
+                                </a>
+                                <span className="flex items-center whitespace-nowrap rounded-md border border-jacarta-100 py-1 px-2">
+                                  <span className="text-sm font-medium tracking-tight text-gray dark:text-white">Sudah Berlalu</span>
+                                </span>
                               </div>
-                            </article>
+                              <h2 className="mb-4 mt-2 font-display text-xl text-jacarta-700 dark:text-white">{item.Title}</h2>
+                              <p className="text-sm text-jacarta-200 dark:text-white">{item.Deskripsi.replace(/<[^>]+>|&nbsp;|&#8203;|[\u200B-\u200D\uFEFF]|-->/g, '').slice(0, 100)}</p>
+                            </div>
                           </div>
-                        ))
-                      ) : (
-                        <p>No agenda found for the selected filters.</p>
-                      )}
-                    </div>
+                        </article>
+                      ))
+                    ) : (
+                      <p>Agenda tidak ditemukan.</p>
+                    )}
                   </div>
                 </div>
               </div>
--- src/controllers/PelayananController.ts
+++ src/controllers/PelayananController.ts
@@ -98,7 +98,7 @@ export async function getLayananKota() {
   //    return LayananKota;
   // }
 
-  const result = await api({ url: `${API_CMS}/ViewPortal/getExLink?siteId=2&code=&groupId=&typeId=LM&limit=3&offset=&parent=` });
+  const result = await api({ url: `${API_CMS}/ViewPortal/getExLink?siteId=2&code=&groupId=&typeId=LM&limit=&offset=&parent=` });
   if ('error' in result) {
     consoleError('get_content()', result.error);
   } else {
@@ -123,7 +123,7 @@ export async function getDetailLayananKota(Id: string) {
   //    return DetailLayananKota;
   // }
 
-  const result = await api({ url: `${API_CMS}/ViewPortal/getExLink?siteId=2&code=&groupId=&typeId=LM&limit=3&offset=&parent=${Id}` });
+  const result = await api({ url: `${API_CMS}/ViewPortal/getExLink?siteId=2&code=&groupId=&typeId=LM&limit=&offset=&parent=${Id}` });
 
   if ('error' in result) {
     consoleError('get_content()', result.error);
